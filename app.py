d0179726f43e875637aa04f4248160df6531277e
satvir singh <sssatvir12@gmail.com>
2022-06-30 09:28:41 +0530

    dynamic binding of routes examples

--- app.py
+++ app.py
@@ -13,5 +13,20 @@ def hello(name):
 @app.route('/hello')
 def hello1():
     return 'Hello, World'
+@app.route('/int/<int:roll_number>')
+def student(roll_number):
+    return f"rollnumber, {escape(roll_number)}!"
+@app.route('/string/<string:name>')
+def student_name(name):
+    return f"name, {escape(name)}!"
+@app.route('/float/<float:fee>')
+def student_fee(fee):
+    return f"fee, {escape(fee)}!"
+@app.route('/path/<path:p>')
+def studentpath(p):
+    return f"path, {escape(p)}!"
+@app.route('/uuid/<uuid:p>')
+def studentuuid(p):
+    return f"uuid, {escape(p)}!"
 if __name__ == '__main__':
     app.run()
